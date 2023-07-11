ce6205c904fc062d7f6be56f5e229e5783dd9925
ricardo-werner <wernerdive@gmail.com>
2023-07-10 21:21:08 -0300

    feat: change folders connection e user

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -4,13 +4,9 @@ config()
 module.exports = {
     dialect: process.env.DIALECT, //Qual banco de dados está utilizando;
     host: process.env.HOST, //Qual servidor está utilizando;
-    username: process.env.USERNAMEDB, //Qual o nome do seu usuário no postgres;
-    password: process.env.PASSWORDDB, //Qual a senha do seu usuário no postgres;
+    username: process.env.DB_USERNAME, //Qual o nome do seu usuário no postgres;
+    password: process.env.DB_PASSWORD, //Qual a senha do seu usuário no postgres;
     database: process.env.DATABASE, //Qual o nome do seu database no postgres;
     port: process.env.PORT, //Qual porta do seu postgres (Normalmente é a 5432);
     secret_key: process.env.SECRET_KEY, //Chave secreta para gerar tokens;
-    define: {
-        underscored: true,
-        underscoredAll: true
-    }
 };
