7a8f8756540afe7512ff3c10793217e457213c95
ricardo-werner <wernerdive@gmail.com>
2023-07-10 21:41:41 -0300

    feat: change database.js

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,6 +1,8 @@
 const { config } = require('dotenv')
 config()
 
+console.log(process.env)
+
 module.exports = {
     dialect: process.env.DIALECT, //Qual banco de dados está utilizando;
     host: process.env.HOST, //Qual servidor está utilizando;
@@ -9,4 +11,8 @@ module.exports = {
     database: process.env.DATABASE, //Qual o nome do seu database no postgres;
     port: process.env.PORT, //Qual porta do seu postgres (Normalmente é a 5432);
     secret_key: process.env.SECRET_KEY, //Chave secreta para gerar tokens;
+    define: {
+        underscored: true,
+        underscoredAll: true,
+    }
 };
