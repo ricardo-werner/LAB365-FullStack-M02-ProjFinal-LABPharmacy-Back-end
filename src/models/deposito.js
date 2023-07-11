32174c2a69792e65f04ddd96f0a81e4a9397bb3b
ricardo-werner <wernerdive@gmail.com>
2023-07-11 17:35:09 -0300

    feat: created deposito migration and models

---
+++ src/models/deposito.js
@@ -0,0 +1,86 @@
+const { INTEGER, STRING, DATE, ENUM } = require('sequelize');
+const { connection } = require('../database/connection');
+
+const Deposito = connection.define("deposito", {
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  usuario_id: {
+    type: INTEGER,
+    allowNull: false,
+    references: { model: 'usuario', key: 'id' }
+  },
+  distribuidor_name: {
+    type: STRING,
+    allowNull: false,
+    unique: true
+  },
+  cnpj: {
+    type: STRING,
+    allowNull: false,
+    unique: true
+  },
+  contato: {
+    type: STRING,
+    allowNull: false
+  },
+  cep: {
+    type: STRING,
+    allowNull: false
+  },
+  endereco: {
+    type: STRING,
+    allowNull: false
+  },
+  bairro: {
+    type: STRING,
+    allowNull: false
+  },
+  cidade: {
+    type: STRING,
+    allowNull: false
+  },
+  estado: {
+    type: STRING,
+    allowNull: false
+  },
+  numero: {
+    type: STRING,
+    allowNull: false
+  },
+  complemento: {
+    type: STRING,
+    allowNull: true
+  },
+  latitude: {
+    type: STRING,
+    allowNull: true
+  },
+  longitude: {
+    type: STRING,
+    allowNull: true
+  },
+  status: {
+    type: ENUM,
+    values: ['ativo', 'inativo'],
+    allowNull: false,
+    defaultValue: 'ativo'
+  },
+  created_at: {
+    type: DATE,
+    allowNull: false
+  },
+  updated_at: {
+    type: DATE,
+    allowNull: false
+  },
+  deleted_at: {
+    type: DATE,
+    allowNull: true
+  },
+}, { underscored: true, paranoid: true })
+
+module.exports = { Deposito }
