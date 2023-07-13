eb353f6f2a4a90f00393f808ed40867e3ff48642
ricardo-werner <wernerdive@gmail.com>
2023-07-13 16:17:24 -0300

    fix:: add underscore and paranoid

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -77,6 +77,6 @@ const Usuario = connection.define("usuario", {
         type: DATE,
         allowNull: true
       },
-  });
+  }, { underscore: true, paranoid: true });
 
 module.exports = { Usuario }
