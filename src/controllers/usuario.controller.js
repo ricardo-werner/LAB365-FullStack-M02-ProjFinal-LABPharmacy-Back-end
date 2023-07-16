4ab06d13b6c457541613f0b743247c10ed05eba0
ricardo-werner <wernerdive@gmail.com>
2023-07-16 01:41:47 -0300

    feat: implement post, get, patch, delete usuario

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -71,6 +71,87 @@ class UsuarioController {
             })
         }
     }
+
+    async listAllUsuarios(request, response) {
+        try {
+            const data = await Usuario.findAll()
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(400).send({
+                message: "Falha na operação de listar usuários",
+                cause: error.message
+            })
+        }
+    }
+
+    async listOneUsuario(request, response) {
+        try {
+            const { id } = request.params
+            const data = await Usuario.findOne({
+                where: { id: id }
+            })
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(400).send({
+                message: "Falha na operação de listar usuário",
+                cause: error.message
+            })
+        }
+    }
+
+    async updateOneUsuario(request, response) {
+        try {
+            const { id } = request.params
+            const {
+                nome,
+                sobrenome,
+                genero,
+                dt_nascimento,
+                cpf,
+                telefone,
+                email,
+                senha,
+                status
+            } = request.body;
+
+            const data = await Usuario.update({
+                nome,
+                sobrenome,
+                genero,
+                dt_nascimento,
+                cpf,
+                telefone,
+                email,
+                senha,
+                status
+            }, {
+                where: { id: id }
+            })
+
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(400).send({
+                message: "Falha na operação de atualizar usuário",
+                cause: error.message
+            })
+        }
+    }
+
+    async deleteOneUsuario(request, response) {
+        try {
+            const { id } = request.params
+            const data = await Usuario.destroy({
+                where: { id: id }
+            })
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(400).send({
+                message: "Falha na operação de deletar usuário",
+                cause: error.message
+            })
+        }
+    }
+
 }
 
 module.exports = new UsuarioController()
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,10 +1,22 @@
-const { createOneUsuario } = require('../controllers/usuario.controller')
+const {
+    createOneUsuario,
+    loginUsuario,
+    listAllUsuarios,
+    listOneUsuario,
+    updateOneUsuario,
+    deleteOneUsuario
+} = require('../controllers/usuario.controller')
 const { Router } = require('express')
 
-class UsuarioRouter{
-    routesFromUsuario () {
-        const usuarioRoutes  = Router()
+class UsuarioRouter {
+    routesFromUsuario() {
+        const usuarioRoutes = Router()
         usuarioRoutes.post('/createOneUsuario', createOneUsuario)
+        usuarioRoutes.get('/loginUsuario', loginUsuario)
+        usuarioRoutes.get('/listAllUsuarios', listAllUsuarios)
+        usuarioRoutes.get('/listOneUsuario/:id', listOneUsuario)
+        usuarioRoutes.patch('/updateOneUsuario/:id', updateOneUsuario)
+        usuarioRoutes.delete('/deleteOneUsuario/:id', deleteOneUsuario)
         return usuarioRoutes
     }
 }
