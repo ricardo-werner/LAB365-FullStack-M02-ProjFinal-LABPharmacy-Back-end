c0738681719e6ebc9cd61d760c671b34ce8fcf06
ricardo-werner <wernerdive@gmail.com>
2023-07-16 23:27:13 -0300

    feat: created CRUD Medicamento and Routes

--- src/controllers/medicamento.controller.js
+++ src/controllers/medicamento.controller.js
@@ -40,6 +40,100 @@ class MedicamentoController {
             )
         }
     }
+
+    async listAllMedicamentos(request, response) {
+        try {
+            const data = await Medicamento.findAll()
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(400).send({
+                message: "Falha na operação de listar medicamentos",
+                cause: error.message
+            })
+        }
+    }
+
+    async listOneMedicamento(request, response) {
+        try {
+            const { id } = request.params
+            const data = await Medicamento.findOne({
+                where: { id: id }
+            })
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(400).send({
+                message: "Falha na operação de listar medicamento",
+                cause: error.message
+            })
+        }
+    }
+
+    async updateOneMedicamento(request, response) {
+        try {
+            const { id } = request.params
+            const {
+                usuario_id,
+                deposito_id,
+                nome_medicamento,
+                nome_laboratorio,
+                descricao_medicamento,
+                dosagem_medicamento,
+                unidade_dosagem,
+                tipo_medicamento,
+                preco_unitario,
+                quantidade
+            } = request.body;
+
+            const data = await Medicamento.update({
+                usuario_id,
+                deposito_id,
+                nome_medicamento,
+                nome_laboratorio,
+                descricao_medicamento,
+                dosagem_medicamento,
+                unidade_dosagem,
+                tipo_medicamento,
+                preco_unitario,
+                quantidade
+            }, {
+                where: { id: id }
+            })
+
+            return response.status(200).send(data)
+        } catch (error) {
+            return response.status(400).send({
+                message: "Falha na operação de atualizar o medicamento",
+                cause: error.message
+            })
+        }
+    }
+
+    async deleteOneMedicamento(req, res) {
+        const { id } = req.params;
+
+        // Buscar o usuário pelo identificador
+        const user = await Medicamento.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ error: 'Medicamento não encontrado' });
+        }
+
+        // Atualizar o campo de status e delete_at
+        if (user.status === 'ativo') {
+            user.status = 'inativo';
+            user.deleted_at = new Date();
+            console.log(user.deleted_at)
+        } else if (user.status === 'inativo') {
+            user.status = 'ativo';
+            user.deleted_at = null;
+        }
+
+        // Salvar a alteração no banco de dados
+        await user.save();
+
+        return res.status(200).json(user);
+    } catch(error) {
+        return res.status(400).json({ error: error.message });
+    }
 }
 
 module.exports = new MedicamentoController()
--- src/routes/medicamento.routes.js
+++ src/routes/medicamento.routes.js
@@ -1,12 +1,22 @@
-const { createOneMedicamento } = require('../controllers/medicamento.controller')
+const {
+    createOneMedicamento,
+    listAllMedicamentos,
+    listOneMedicamento,
+    updateOneMedicamento,
+    deleteOneMedicamento
+} = require('../controllers/medicamento.controller')
 const { Router } = require('express')
+const { auth } = require('../middleware/auth')
 
-class MedicamentoRouter{
-    routesFromMedicamento () {
-        const medicamentoRoutes  = Router()
-        medicamentoRoutes.post('/createOneMedicamento', createOneMedicamento)
+class MedicamentoRouter {
+    routesFromMedicamento() {
+        const medicamentoRoutes = Router()
+        medicamentoRoutes.post('/createOneMedicamento', auth, createOneMedicamento)
+        medicamentoRoutes.get('/listAllMedicamentos', auth, listAllMedicamentos)
+        medicamentoRoutes.get('/listOneMedicamento/:id', auth, listOneMedicamento)
+        medicamentoRoutes.patch('/updateOneMedicamento/:id', auth, updateOneMedicamento)
+        medicamentoRoutes.delete('/deleteOneMedicamento/:id', auth, deleteOneMedicamento)
         return medicamentoRoutes
     }
 }
-
 module.exports = new MedicamentoRouter()
